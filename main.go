e1f9fe46b68c91e227c7348290d47fba3fc35251
Karan Sharma <karansharmab07@gmail.com>
2024-05-29 12:18:10 +0530

    user attributes declared

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
-	var conferenceName string = "Go Conference" //in golang if you declare the var , then you need use it  ELES will get error
+	var conferenceName string = "Go Conference" //in golang if you declare the var , then you need use it  ELES will get error // := easy syntax can be called as synthetic sugar
 	//fmt.Println(conferenceName)
 
 	const conferenceTickets int = 50
@@ -16,12 +18,34 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v  are still available.\n", conferenceTickets, remainingTickets) //println - for new line and
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	//user
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
-	//ask user for their name
 
-	userName = "Keera"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets) //%v or fmt placeholder works with fmt.Printf  -IMP*
+	//ask user for their name
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your Last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your booked ticktes:")
+	fmt.Scan(&userTickets)
+
+	fmt.Println("Enter your Email address:")
+	fmt.Scan(&email)
+
+	// fmt.Scan(&firstName)    //user input funtion //&variable name -it helps to get input and so user can get desired outpur by giving input
+	//user input funtion //&-pointer help to get input
+
+	//RUF
+	// fmt.Println(remainingTickets)
+	// fmt.Println(&remainingTickets) //pointer
+
+	// userName = "Keera"
+	// userTickets = 2
+	fmt.Printf("User  %v  %v booked %v tickets and his Email addres %v.\n", firstName, lastName, userTickets, email) //%v or fmt placeholder works with fmt.Printf  -IMP*
 
 }
